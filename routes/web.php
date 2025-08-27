ee5145c126f7ef09400d8be4f8e4c36e1e1d1be4
tonealexguerra <tone.alex.guerra@gmail.com>
2025-08-27 16:49:10 -0300

    Update routes

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('tasks', TaskController::class);
+//Route::resource('tasks', TaskController::class);
+
+Route::prefix('tasks')->name('tasks.')->group(function () {
+    Route::get('/', [TaskController::class, 'index'])->name('index');        // Listar tarefas
+    Route::get('/create', [TaskController::class, 'create'])->name('create'); // Formulário de criação
+    Route::post('/', [TaskController::class, 'store'])->name('store');        // Salvar nova tarefa
+    Route::get('/{task}/edit', [TaskController::class, 'edit'])->name('edit'); // Formulário de edição
+    Route::put('/{task}', [TaskController::class, 'update'])->name('update');  // Atualizar tarefa
+    Route::delete('/{task}', [TaskController::class, 'destroy'])->name('destroy'); // Remover tarefa
+});
+
+
+
 
